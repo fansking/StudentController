b5f8f42e76106a97a90d56517fd7ea612f187e53
1033416889 <1033416889@qq.com>
2019-03-19 14:09:32 +0800

    查询模版

--- src/test/java/com/myjpa/springboot/ApplicationTests.java
+++ src/test/java/com/myjpa/springboot/ApplicationTests.java
@@ -32,39 +32,42 @@ public class ApplicationTests {
 
     @Test
     public void init() {
-        Athlete athlete1 = new Athlete("001",12,"张三",
-                "123235345","哈哈哈",false,
-                99,"",new HashSet<AthleteCompetition>());
-        Athlete athlete2 = new Athlete("002",12,"老王",
-                "123242345345","哈哈哈",false,
-                99,"",new HashSet<AthleteCompetition>());
+        Athlete athlete1 = new Athlete("001", 12, "张三",
+                "123235345", "哈哈哈", false,
+                99, "", new HashSet<AthleteCompetition>());
+        Athlete athlete2 = new Athlete("002", 12, "老王",
+                "123242345345", "哈哈哈", false,
+                99, "", new HashSet<AthleteCompetition>());
 
-        Competition competition1 = new Competition("hahah",12,
-                false,true,new HashSet<AthleteCompetition>());
-
-
-        AthleteCompetition athleteCompetition1 = new AthleteCompetition(athlete1,competition1,1,0,0);
-        AthleteCompetition athleteCompetition2 = new AthleteCompetition(athlete2,competition1,1,0,0);
+        Competition competition1 = new Competition("hahah", 12,
+                false, true, new HashSet<AthleteCompetition>());
 
 
+        AthleteCompetition athleteCompetition1 = new AthleteCompetition(athlete1, competition1, 1, 0, 0);
+        AthleteCompetition athleteCompetition2 = new AthleteCompetition(athlete2, competition1, 1, 0, 0);
 
-        competition1.getAthleteCompetitions().addAll(Arrays.asList(athleteCompetition2,athleteCompetition1));
+
+        competition1.getAthleteCompetitions().addAll(Arrays.asList(athleteCompetition2, athleteCompetition1));
 
         Team team1 = new Team();
-        team1.setAthletes(Arrays.asList(athlete1,athlete2));
+        team1.setAthletes(Arrays.asList(athlete1, athlete2));
         athlete1.setTeam(team1);
         athlete2.setTeam(team1);
-        teamRepository.save(team1);
+        team1.setName("asd");
+        // teamRepository.save(team1);
 //        athlete1.setId(8);
 //        athlete2.setId(10);
 
         athlete1.getAthleteCompetitions().add(athleteCompetition1);
         athlete2.getAthleteCompetitions().add(athleteCompetition2);
 
+        //  List<Team> all = teamRepository.findAll();
+        Team asd = teamRepository.findTeamByName("asd");
+
         competitionRepository.save(competition1);
         athleteRepository.save(athlete1);
         athleteRepository.save(athlete2);
-        athleteCompetitionRepository.save(new AthleteCompetition(athlete1,competition1,99,1,1));
+        athleteCompetitionRepository.save(new AthleteCompetition(athlete1, competition1, 99, 1, 1));
 
 
     }
